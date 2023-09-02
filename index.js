d312f5a888301b8fc2407f294058af08c0df442b
Saroj2004 <105579570+Saroj2004@users.noreply.github.com>
2023-09-02 15:25:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let signinbutton = document.getElementById("signinbutton");
+signinbutton.onclick=function(){
+
+document.getElementById("maintext").textContent = "Just wait...";
+document.getElementById("maintext").style.color="#FF4B2B";
+
+}
